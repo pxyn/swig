8b6a73051e59496b1e083528f2b45bc0d49f675c
Olly Betts <olly@survex.com>
2024-02-23 12:53:14 +1300

    [ci] Drop ocaml 4.08.1 job
    It seems ocaml generates code which GCC rejects so not a SWIG problem.

--- Tools/testflags.py
+++ Tools/testflags.py
@@ -17,7 +17,7 @@ def get_cflags(language, std, compiler):
     "javascript":"-Werror " + c_common,
            "lua":"-Werror " + c_common,
       "mzscheme":"-Werror " + c_common,
-         "ocaml":"-Werror " + c_common + " -fpermissive -Wno-error=narrowing", # Needed for 4.08.1, not for 4.13.1
+         "ocaml":"-Werror " + c_common,
         "octave":"-Werror " + c_common,
          "perl5":"-Werror " + c_common,
            "php":"-Werror " + c_common,
@@ -48,7 +48,7 @@ def get_cxxflags(language, std, compiler):
     "javascript":"-Werror " + cxx_common + " -Wno-error=unused-function", # Until overload_rename is fixed for node
            "lua":"-Werror " + cxx_common,
       "mzscheme":"-Werror " + cxx_common,
-         "ocaml":"-Werror " + cxx_common + " -fpermissive -Wno-error=narrowing", # Needed for 4.08.1, not for 4.13.1
+         "ocaml":"-Werror " + cxx_common,
         "octave":"-Werror " + cxx_common,
          "perl5":"-Werror " + cxx_common,
            "php":"-Werror " + cxx_common,
